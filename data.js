898877da0d591e72a48cc61da5a3811fe664a6a4
Damien <34070001+DefinitelyNotGitHubOfficial@users.noreply.github.com>
2017-11-28 11:34:52 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+var jsonData = 
+    {
+        "Films": [
+            {
+                "Name": "Alien",
+            	"Date": 1979,
+                "Sub": "Science Fiction",
+				"Era": "70s",
+				"Director": "Ridley Scott",
+				"Starring":[
+					 "Sigourney Weaver",
+					 "Tom Skerritt",
+					 "Veronica Cartwright",
+					 "Harry Dean Stanton",
+					 "John Hurt",
+					 "Ian Holm",
+					 "Yaphet Kotto",
+					 "Bolaji Badejo"
+					],
+				"Tags":[
+					"Creature",
+					"thriller",
+					"isolation",
+					"Science Fiction",
+					"technology"
+				]
+           },{
+		   "Name": "Boogy Manus",
+            	"Date": 1984,
+                "Sub": "Science Fiction",
+				"Era": "80s",
+				"Director": "Ridley Scott",
+				"Starring":[
+					 "Sigourney Weaver",
+					 "Tom Skerritt",
+					 "Veronica Cartwright",
+					 "Harry Dean Stanton",
+					 "John Hurt",
+					 "Ian Holm",
+					 "Yaphet Kotto",
+					 "Bolaji Badejo"
+					],
+				"Tags":[
+					"Creature",
+					"thriller",
+					"isolation",
+					"Fantasy",
+					"technology"
+				]
+		   
+		   }]}
